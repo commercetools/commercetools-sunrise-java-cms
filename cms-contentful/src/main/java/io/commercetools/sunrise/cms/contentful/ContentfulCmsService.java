87ec55f9314d26b448565b01bd9188935c9da6de
Dariusz Orlowski <dariusz.orlowski@combc.pl>
2016-06-02 08:31:13 +0200

    Use asynchronous call to fetch data

--- cms-contentful/src/main/java/io/commercetools/sunrise/cms/contentful/ContentfulCmsService.java
+++ cms-contentful/src/main/java/io/commercetools/sunrise/cms/contentful/ContentfulCmsService.java
@@ -2,6 +2,7 @@ package io.commercetools.sunrise.cms.contentful;
 
 import com.contentful.java.cda.CDAArray;
 import com.contentful.java.cda.CDAAsset;
+import com.contentful.java.cda.CDACallback;
 import com.contentful.java.cda.CDAClient;
 import com.contentful.java.cda.CDAEntry;
 import io.commercetools.sunrise.cms.CmsIdentifier;
@@ -30,21 +31,36 @@ public class ContentfulCmsService implements CmsService {
 
     @Override
     public CompletionStage<Optional<String>> get(final List<Locale> locales, final CmsIdentifier cmsIdentifier) {
-        return CompletableFuture.supplyAsync(() -> getEntry(cmsIdentifier)
-                .map(entry -> getLocalizedField(locales, entry, cmsIdentifier.getFieldName()))
-                .orElseGet(Optional::empty));
+        return getEntry(cmsIdentifier)
+                .thenApplyAsync(cdaEntryOptional -> cdaEntryOptional
+                        .flatMap(cdaEntry -> getLocalizedField(locales, cdaEntry, cmsIdentifier.getFieldName())));
     }
 
-    // TODO create models
-    private Optional<CDAEntry> getEntry(final CmsIdentifier cmsIdentifier) {
-        final CDAArray cdaArray = client.fetch(CDAEntry.class)
+    private CompletionStage<Optional<CDAEntry>> getEntry(final CmsIdentifier cmsIdentifier) {
+        return fetchEntry(cmsIdentifier).thenApply(cdaArray -> Optional.ofNullable(cdaArray)
+                .filter(e -> e.items() != null && !e.items().isEmpty())
+                .map(e -> (CDAEntry) e.items().get(0)));
+    }
+
+    private CompletionStage<CDAArray> fetchEntry(final CmsIdentifier cmsIdentifier) {
+        final CompletableFuture<CDAArray> future = new CompletableFuture<>();
+        final CDACallback<CDAArray> callback = new CDACallback<CDAArray>(){
+            @Override
+            protected void onSuccess(final CDAArray result) {
+                future.complete(result);
+            }
+
+            @Override
+            protected void onFailure(final Throwable error) {
+                future.completeExceptionally(error);
+            }
+        };
+        client.fetch(CDAEntry.class)
                 .where(ENTRY_TYPE, cmsIdentifier.getEntryType())
                 .where(ENTRY_KEY, cmsIdentifier.getEntryKey())
                 .where(LIMIT, ONE)
-                .all();
-        return Optional.ofNullable(cdaArray)
-                .filter(e -> e.items() != null && !e.items().isEmpty())
-                .map(e -> (CDAEntry) e.items().get(0));
+                .all(callback);
+        return future;
     }
 
     Optional<String> getLocalizedField(final List<Locale> locales, final CDAEntry cdaEntry, final String fieldName) {
